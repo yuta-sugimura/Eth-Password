d38b213726631c5dcd898f979555b003ebc4ab95
yuta-sugimura <36661726+yuta-sugimura@users.noreply.github.com>
2018-08-12 14:54:26 +0900

    truffle.js

--- truffle.js
+++ truffle.js
@@ -1,12 +1,14 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "mnemonic";
-
+let HDWalletProvider = require("truffle-hdwallet-provider")
+let mnemonic = "12 words";
+let infura_ropsten = "https://ropsten.infura.io/v3/"
+let infura_main = "https://mainnet.infura.io/v3/"
+let infura_key = ""
 
 module.exports = {
   networks: {
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/_id")
+        return new HDWalletProvider(mnemonic, infura_ropsten + infura_key)
       },
       network_id: 3,
       gas: 3000000,
@@ -15,17 +17,17 @@ module.exports = {
 
     "live": {
       provider: function() {
-        return new HDWalletProvider(mnemonic, "https://mainnet.infura.io/_id")
+        return new HDWalletProvider(mnemonic, infura_main + infura_key)
       },
       network_id: 1,
-      gas: 4000000,
-      gasPrice: 8000000000
+      gas: 3000000,
+      gasPrice: 5000000000
     },
 
     development: {
       host: '127.0.0.1',
       port: 7545,
-      network_id: '*'
+      network_id: '*' // Match any network id
     }
   }
 };
